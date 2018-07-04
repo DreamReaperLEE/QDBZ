36f65927c9b371a8708573fd66e6534cc24d507b
Sumail-Lee <lee.shouzheng@gmail.com>
2018-07-04 11:06:15 +0800

    首页底部

--- shop/templates/default/footer_index.php
+++ shop/templates/default/footer_index.php
@@ -30,12 +30,12 @@
             <div class="col-md-4 col-lg-4 col-sm-4 " style="padding:0;">
                 <h3 class="weui-f18">联系我们</h3>
                 <ul class="bgn">
-                    <li><i></i> <a href="#" class="weui-v_m weui-t_d">网址主页：、www.jmrhip.com/</a></li>
-                    <li><i class="fbg1"></i> <span class="weui-v_m">联 系 人 ：周先生</span></li>
-                    <li><i class="fbg2"></i> <span class="weui-v_m">联系邮箱：admin@163.com</span></li>
-                    <li><i class="fbg3"></i> <span class="weui-v_m">手机号码：185 0381 1955</span></li>
-                    <li><i class="fbg4"></i> <span class="weui-v_m">传真号码：0512-888 888 88</span></li>
-                    <li><i class="fbg5"></i> <span class="weui-v_m">地       址：陕西省西安市雁塔区锦业路1号都市之门B座11701室</span></li>
+                    <li><i></i> <a href="bz.heuqst.cn" class="weui-v_m weui-t_d">网址主页：bz.heuqst.cn</a></li>
+                    <li><i class="fbg1"></i> <span class="weui-v_m">联 系 人 ：李奕霏</span></li>
+                    <li><i class="fbg2"></i> <span class="weui-v_m">联系邮箱：xmgl@heuqst.cn</span></li>
+                    <li><i class="fbg3"></i> <span class="weui-v_m">手机号码：135 8922 8865</span></li>
+                    <li><i class="fbg4"></i> <span class="weui-v_m">传真号码：0532-58707871</span></li>
+                    <li><i class="fbg5"></i> <span class="weui-v_m">地    址：中国 青岛 黄岛区科教二路167号</span></li>
                 </ul>
             </div>
             <div class="col-md-2 col-lg-2 col-sm-2 weui-t_r">
@@ -47,7 +47,7 @@
         </div>
     </div>
     <div class="footer_b weui-t_c">
-        Copyright © 2016-2018 西安科技大市场创新云服务股份有限公司 版权所有 陕ICP备16006780号-2<br>陕西省西安市雁塔区锦业路1号都市之门B座11701室
+        Copyright © 青岛古镇口军民融合舰船装备技术保障有限公司 版权所有 鲁ICP备18004863号<br>中国 青岛 黄岛区科教二路167号
     </div>
 </div>
 
